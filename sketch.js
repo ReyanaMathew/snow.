d7bd6e1a27352f2b648e80433a03ba71946dc5ff
ReyanaMathew <74352665+ReyanaMathew@users.noreply.github.com>
2021-06-06 12:28:09 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+var bg;
+
+var maxSnow=100;
+var snow=[];
+var rand;
+function preload() {
+  bg=loadImage("snow3.jpg");
+}
+function setup() {
+  createCanvas(800,400)
+}
+
+function draw() {
+  background(bg)
+  rand = Math.round(random(1,4));
+
+if(frameCount%5===0) {
+    snow.push(new Snow(random(0, 800), 30, 30))
+    
+  }
+  for(var j = 0; j<snow.length; j++) {
+    snow[j].display()
+  }
+  drawSprites();
+}
